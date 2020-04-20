9e595430f5d759cfcd20ac905998dcb996acec7c
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-04-20 02:39:46 +0200

    first version welcome page

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+  // console.log(window.location.pathname);
+
+  if(window.location.pathname === '/public/'){
+    const welcomePane = document.getElementById('js--welcome-pane');
+    const welcomeReception = document.getElementById('js--welcome-reception');
+    const welcomeBackground = document.getElementById('js--welcome-background');
+    console.log(welcomeBackground.style.objectPosition);
+
+    testFunction = (e) => {
+      console.log(e);
+    }
+
+    function detectMouse(e) {
+      let offsetPaneX = 910;
+      let offsetPaneY = 460;
+      let offsetReceptionX = 870;
+      let offsetReceptionY = 435;
+      let paneModifier = 20;
+      let receptionModifier = 12;
+
+      welcomePane.style.left = e.x/paneModifier + offsetPaneX + "px";
+      welcomePane.style.top = e.y/paneModifier + offsetPaneY + "px";
+
+      welcomeReception.style.left = e.x/receptionModifier + offsetReceptionX + "px";
+      welcomeReception.style.top = e.y/receptionModifier + offsetReceptionY + "px";
+
+      welcomeBackground.style.objectPosition = (e.x/80) + "% " +  ((e.y/350)+70) + "% ";
+      welcomeBackground.style.left = (-e.x/375) + "%";
+      welcomeBackground.style.top = (-e.y/220) + "%";
+    }
+
+    welcomePane.addEventListener("click", testFunction);
+
+    document.addEventListener("mousemove", detectMouse);
+
+  }
+
+}
