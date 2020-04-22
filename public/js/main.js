e82148bae3a2c897ff517c23e754d284d18c959b
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-04-22 21:49:55 +0200

    first version me page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,50 @@
 window.onload = function(){
   // console.log(window.location.pathname);
+  const viewPortWidth = document.documentElement.clientWidth;
+  const viewPortHeight = document.documentElement.clientHeight;
+  // alert(viewPortWidth + ", " + viewPortHeight);
+  // 1903 969
+
+  if($("body").data("page") !== "index"){
+    const firstBar = document.getElementById("js--firstBar");
+    const secondBar = document.getElementById("js--secondBar");
+    const thirdBar = document.getElementById("js--thirdBar");
+    const barContainer = document.getElementById("js--barContainer");
+    const barContainerBg = document.getElementById("js--barContainerBg");
+    const navbarList = document.getElementById('js--navbar-list');
+    const navbarWork = document.getElementById('js--navbar-work');
+    const navbarProfile = document.getElementById('js--navbar-profile');
+
+    let menuBool = false;
+
+    barContainer.onmousedown = function (){
+      firstBar.classList.toggle("change");
+      secondBar.classList.toggle("change");
+      thirdBar.classList.toggle("change");
+      let timeoutTime;
+
+      if (menuBool === false) {
+        timeoutTime = 100;
+        menuBool = !menuBool;
+        // console.log(menuBool);
+      } else {
+        timeoutTime = 40;
+        menuBool = !menuBool;
+        // console.log(menuBool);
+      }
+
+      barContainerBg.classList.toggle("change");
+
+      navbarList.classList.toggle("roll");
+
+      setTimeout(function(){
+        navbarWork.classList.toggle("roll");
+        setTimeout(function(){
+          navbarProfile.classList.toggle("roll");
+        }, timeoutTime);
+      }, timeoutTime);
+    }
+  }
 
   if($("body").data("page") === "index"){
     const welcomePane = document.getElementById('js--welcome-pane');
@@ -51,14 +96,20 @@ window.onload = function(){
     const navbarList = document.getElementById('js--navbar-list');
     const navbarWork = document.getElementById('js--navbar-work');
     const navbarProfile = document.getElementById('js--navbar-profile');
+
     const workIpmedt2Clickbait = document.getElementsByClassName('cb1')[0];
     const workIipmedtClickbait = document.getElementsByClassName('cb2')[0];
     const workIpmedt3Clickbait = document.getElementsByClassName('cb3')[0];
     const workIpmedt5Clickbait = document.getElementsByClassName('cb4')[0];
 
+    const workIpmedt2Thumb = document.getElementById('js--ipmedt2');
+    const suiniflow = 'https://suiniflow.firebaseapp.com/';
+
     const workThumb = document.getElementsByClassName('work__pair-thumb');
 
-    let menuBool = false;
+    workIpmedt2Thumb.onmousedown = () => {
+      window.location.href = suiniflow;
+    }
 
     displayClickbait = (elem) => {
       // console.log(elem.classList[1]);
@@ -97,33 +148,45 @@ window.onload = function(){
       }
     }
 
-    barContainer.onmousedown = function (){
-      firstBar.classList.toggle("change");
-      secondBar.classList.toggle("change");
-      thirdBar.classList.toggle("change");
-      let timeoutTime;
 
-      if (menuBool === false) {
-        timeoutTime = 100;
-        menuBool = !menuBool;
-        // console.log(menuBool);
-      } else {
-        timeoutTime = 40;
-        menuBool = !menuBool;
-        // console.log(menuBool);
+  }else if ($("body").data("page") === "me"){
+    const myPicture = document.getElementById('js--my-picture');
+
+    // onclick
+    // change image
+    // spin container
+    let pictureIndex = 1;
+    let myClassesArray = ['yaboi1', 'yaboi2', 'yaboi3']
+    // let myPictureArray = ['../media/homeboy.jpg', '', ''];
+
+    changeMyPicture = () => {
+      // myPicture.classList.add(myClassesArray[pictureIndex]);
+      // myPicture.classList.remove(myClassesArray[pictureIndex-1]);
+      switch (pictureIndex) {
+        case 0:
+          myPicture.classList.add(myClassesArray[pictureIndex]);
+          myPicture.classList.remove(myClassesArray[2]);
+          break;
+        case 1:
+          myPicture.classList.add(myClassesArray[pictureIndex]);
+          myPicture.classList.remove(myClassesArray[0]);
+          break;
+        case 2:
+          myPicture.classList.add(myClassesArray[pictureIndex]);
+          myPicture.classList.remove(myClassesArray[1]);
+          break;
+        default:
+
       }
-
-      barContainerBg.classList.toggle("change");
-
-      navbarList.classList.toggle("roll");
-
-      setTimeout(function(){
-        navbarWork.classList.toggle("roll");
-        setTimeout(function(){
-          navbarProfile.classList.toggle("roll");
-        }, timeoutTime);
-      }, timeoutTime);
+      pictureIndex++;
+      if (pictureIndex>2) {
+        pictureIndex = 0;
+      }
+      console.log("hey");
     }
+
+    myPicture.addEventListener("click", changeMyPicture);
+    // myPicture.addEventListener("mouseup", changeMyPicture);
   }
 
 }
