313e595d1f236cfff3c22b448eef3f1b4ebd3ca3
Amos <amos.peperkamp@gmail.com>
2025-04-29 18:09:44 +0200

    Implemented square toggle all button

--- public/js/experience.js
+++ public/js/experience.js
@@ -7,8 +7,24 @@ logInfo("loaded experience.js file");
 
 if (true) {
     const experienceItems = document.getElementsByClassName('experience__row__item__square');
+    const experienceItemsToggle = document.getElementById('js--experience-squares-toggle');
 
-    for (let i = 0; i < experienceItems.length; i++) {
+    let isOpen = false;
+
+    experienceItemsToggle.onclick = () => {
+        for (let i = 1; i < experienceItems.length; i++) {
+            if (isOpen) {
+                experienceItems[i].classList.add("pulse");
+                experienceItems[i].classList.remove("open");
+            } else {
+                experienceItems[i].classList.remove("pulse");
+                experienceItems[i].classList.add("open");
+            }
+        }
+        isOpen = !isOpen;
+    }
+
+    for (let i = 1; i < experienceItems.length; i++) {
         const item = experienceItems[i];
         item.onclick = () => {
             item.classList.toggle("pulse");
