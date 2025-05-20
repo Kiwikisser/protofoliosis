29c636490782a1845d8a6ada704f8885ab978d9c
Amos <amos.peperkamp@gmail.com>
2025-05-20 17:23:49 +0200

    Start on fancy project product link element

--- public/js/project.js
+++ public/js/project.js
@@ -7,11 +7,13 @@ logInfo("loaded project.js file");
 
 if (true) { // this causes unused variables to be highlighted
     informationInteraction();
+    projectPortal();
 }
 
 function informationInteraction(){
     const qualityItemContainers = document.getElementsByClassName('project__info__item__square-container');
     const qualityItemSquares = document.getElementsByClassName('project__info__item__square');
+    const qualityItemImages = document.getElementsByClassName('project__info__item__square__image');
     const qualityItemText = document.getElementsByClassName('project__info__item__text');
 
     for (let i = 0; i < qualityItemSquares.length; i++) {
@@ -19,6 +21,7 @@ function informationInteraction(){
             qualityItemText[i].classList.toggle('active');
             qualityItemSquares[i].classList.toggle('active');
             qualityItemContainers[i].classList.toggle('active');
+            qualityItemImages[i].classList.toggle('active');
 
             if(qualityItemText[i].classList.contains('active')){
                 setTimeout( () => {
@@ -29,4 +32,28 @@ function informationInteraction(){
             }
         });
     }
+}
+
+function projectPortal(){
+    const projectContent = document.getElementById('js--project-content');
+    const projectPortal = document.getElementById('js--project-portal');
+    const projectPortalImage = document.getElementById('js--project-portal-image');
+
+    projectContent.addEventListener("click", function() {
+        /**
+         * .1s 
+            content rotate 0deg
+
+            .1s-.2s 
+            left arrow more left
+            right arrow more right
+            (just increase content width to twice the size?)
+            Portal widens too 1.5 times
+
+            .2s+
+            Opacity thumbnail 100%
+        */
+    }
+    
+    
 }
